d419848cf19acbb435794deb0b79ba133dc93368
Shelby Trinca <shelby.trinca@gmail.com>
2018-08-06 21:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   let sentences = [];
   for (let i = 0; i < musicians.length; i++) {
-    push.sentences(`${musicians[i]} plays ${instruments[i]}`);
+    sentences.push(`${musicians[i]} plays ${instruments[i]}`);
     }
 }
